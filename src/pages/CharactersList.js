62b90a461add170bbc3713e57bd19e44b5780675
LeoKnox <LeoKnox@users.noreply.github.com>
2019-08-03 15:37:19 -0700

    Add files via upload
    Added map to characters to display character list and link to page.

--- src/pages/CharactersList.js
+++ src/pages/CharactersList.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import characterContent from './character-content'
+import { Link } from 'react-router-dom'
 
-const CharactersList = () => (
-    <>
-        <h1>Characters</h1>
-    </>
-);
+const CharactersList = () => {
+    return (
+        <>
+            <h1>Characters</h1>
+            {characterContent.map((char, key) => (
+                <Link to={`/character/${char.name}`} key={key}>
+                    {char.name} : {char.class}
+                </Link>
+            ))}
+        </>
+    );
+};
 
 export default CharactersList;
